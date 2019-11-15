c545ed9a7f9082bb994b9bc532763b4327660233
luiz787 <diasbertoluiz@gmail.com>
2019-11-15 15:14:24 -0300

    Implementa leitura da entrada.

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include "Graph.hpp"
+
+std::ifstream openInputFile(int argc, char** argv);
+
+int main(int argc, char**argv) {
+    std::cout << "Hello, world!";
+    auto inputFile = openInputFile(argc, argv);
+    uint32_t tableSize;
+    uint32_t columns;
+    uint32_t rows;
+    inputFile >> tableSize >> columns >> rows;
+    Graph graph = Graph(columns * rows);
+    for (uint32_t i = 0; i < columns; i++) {
+        for (uint32_t j = 0; j < rows; i++) {
+            uint32_t value;
+            inputFile >> value;
+        }
+    }
+    return 0;
+}
+
+std::ifstream openInputFile(int argc, char** argv) {
+    if (argc < 2) {
+        throw std::invalid_argument("Nome do arquivo de entrada deve ser passado como argumento.");
+    }
+    const char* inputFileName = argv[1];
+    std::ifstream inputFile (inputFileName);
+    if (!inputFile.is_open()) {
+        throw std::invalid_argument("Falha ao abrir o arquivo de entrada.");
+    }
+    return inputFile;
+}
