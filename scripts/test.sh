6daa265642756bfa1b58542210d27b28d699fc22
luiz787 <diasbertoluiz@gmail.com>
2019-11-17 20:37:26 -0300

    Altera script de teste para contemplar todos os casos de testes.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,16 @@
 cd ../
 mkdir -p tests/output
+rm tests/output
 echo -e "Fazendo a limpeza dos objetos e executáveis...\n"
 make clean
 echo -e "Iniciando compilação...\n"
 make
 echo -e "\nIniciando testes..."
 
-declare -a tests=("221" 
-                "222"
-				"223"
-                "224"
-				"225"
+declare -a tests=("221" "222" "223" "224" "225"
+				"661" "662" "663" "664" "665"
+				"881" "882" "883" "884" "885"
+				"991" "992" "993" "994" "995"
                 )
 
 for i in "${tests[@]}"
